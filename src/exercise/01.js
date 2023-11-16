5d41a2067e18da8d9116b64bff9a3a7bf61a0a5b
ian <ianfarr44@gmail.com>
2023-11-16 08:11:11 -0500

    controlled input

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -1,7 +1,7 @@
 import * as React from 'react'
 
-function Greeting() {
-  const [name, setName] = React.useState('')
+function Greeting({initialName = ''}) {
+  const [name, setName] = React.useState(initialName)
 
   function handleChange(event) {
     setName(event.target.value)
@@ -11,7 +11,7 @@ function Greeting() {
     <div>
       <form>
         <label htmlFor="name">Name: </label>
-        <input onChange={handleChange} id="name" />
+        <input value={name} onChange={handleChange} id="name" />
       </form>
       {name ? <strong>Hello {name}</strong> : 'Please type your name'}
     </div>
